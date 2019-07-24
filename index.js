2c1d1a28f76f91c1189a821e860f52847e978f3c
Franky Liu <cuifranky95@gmail.com>
2019-07-24 02:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-function destructivelyAppendKitten (kittens, element) {
- kittens = kittens.push(element);
+function destructivelyAppendKitten (kittens) {
+ kittens = kittens.push();
   return kittens;
 }
